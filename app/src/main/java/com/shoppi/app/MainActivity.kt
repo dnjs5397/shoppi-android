7b9f8fe071d96adafc62ebe874e26618e357071f
dnjs5397 <dnjs5397@naver.com>
2023-03-07 23:03:19 +0900

    SplashActivity xml설정 -> MainActivity 로직 구현

--- app/src/main/java/com/shoppi/app/MainActivity.kt
+++ app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.shoppi.app
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
+        Log.d("abc", "onCreate")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("abc", "onRestart")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("abc", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("abc", "onResume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("abc", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("abc", "onDestroy")
     }
 }
